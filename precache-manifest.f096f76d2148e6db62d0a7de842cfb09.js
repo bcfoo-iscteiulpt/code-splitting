b8bdc55116f6e72ba4d647fe0000647666656318
Ben <ben_hur_cf@icloud.com>
2019-10-29 23:59:15 +0000

    Updates

---
+++ precache-manifest.f096f76d2148e6db62d0a7de842cfb09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb4c2959a3641d73eb38304cd98154d",
+    "url": "/bcfoo-iscteiulpt/code-splitting/index.html"
+  },
+  {
+    "revision": "5134e8b5cf1bb7bfb975",
+    "url": "/bcfoo-iscteiulpt/code-splitting/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d3c4d9da4b7818df6956",
+    "url": "/bcfoo-iscteiulpt/code-splitting/static/js/2.2447653b.chunk.js"
+  },
+  {
+    "revision": "eb3b88a188513d6bc908",
+    "url": "/bcfoo-iscteiulpt/code-splitting/static/js/3.b662ba04.chunk.js"
+  },
+  {
+    "revision": "ab1aef6b2d00b297b6e0",
+    "url": "/bcfoo-iscteiulpt/code-splitting/static/js/4.aa617459.chunk.js"
+  },
+  {
+    "revision": "5134e8b5cf1bb7bfb975",
+    "url": "/bcfoo-iscteiulpt/code-splitting/static/js/main.65397034.chunk.js"
+  },
+  {
+    "revision": "2b114140374a44e9f96b",
+    "url": "/bcfoo-iscteiulpt/code-splitting/static/js/runtime-main.b0c3c342.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/bcfoo-iscteiulpt/code-splitting/static/media/logo.25bf045c.svg"
+  }
+]);
