044409281de49a1e7cd39a336a4489e584183bff
Ben <ben_hur_cf@icloud.com>
2019-10-31 13:44:54 +0000

    minor

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import AsyncComponent from './components/AsyncComponent';
 function App() {
   const [route, setRoute] = useState('page1')
   const onRouteChange = useCallback(newRoute => setRoute(newRoute), [])
+  const path2 = useCallback(()=>import('./components/Page2'), [])
+  const path3 = useCallback(()=>import('./components/Page3'), [])
   const pagesConfig = {onRouteChange, fallback: <div>Loading...</div>}
   switch (route) {
     case 'page1':
       return <Page1 {...pagesConfig} />
     case 'page2':
       return AsyncComponent({
-        pathCall: ()=>import('./components/Page2'),
+        pathCall: path2,
         ...pagesConfig
       })
     case 'page3':
       return AsyncComponent({
-        pathCall: ()=>import('./components/Page3'),
+        pathCall: path3,
         ...pagesConfig
       })
     default:
